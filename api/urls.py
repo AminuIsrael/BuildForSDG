3d0f226e12bfe284ef1d83b850a343f77c1f47af
israel <aminuisrael2@gmail.com>
2020-04-28 10:23:53 +0000

    added user registration

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path('',views.index_page),
+    path('register',views.user_registration)
 ]
--- api/views.py
+++ api/views.py
@@ -12,5 +12,28 @@ def index_page(request):
     }
     return Response(return_data)
 
+@api_view(["POST"])
+def user_registration(request):
+    try:
+        firstName = request.data.get('firstname',None)
+        lastName = request.data.get('lastname',None)
+        phoneNumber = request.data.get('phonenumber',None)
+        email = request.data.get('email',None)
+        password = request.data.get('password',None)
+        address = request.data.get('address',None)
+        lga = request.data.get('lga',None)
+        state = request.data.get('state',None)
+        country = request.data.get('country',None)
+        reg_field = [firstName,lastName,phoneNumber,email,password,address,lga,state,country]
+        if not None in reg_field and not "" in reg_field:
+            pass
+            
+
+    except Exception as e:
+        return_data = {
+            "error": 2,
+            "message": str(e)
+        }
+
 
 

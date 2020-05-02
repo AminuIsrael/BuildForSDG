d3bc96e8bc05563c5344e6057869191abd581d11
israel <aminuisrael2@gmail.com>
2020-05-02 22:42:21 +0000

    added verification endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views
 urlpatterns = [
     path('',views.index_page),
     path('api/v1/signup',views.user_registration),
-    path('api/v1/signin',views.user_login)
+    path('api/v1/signin',views.user_login),
+    path('api/v1/verification',views.user_verification)
 ]

daf65c38c285d49ae6f88cb0827f7c22bd4cb8b4
israel <aminuisrael2@gmail.com>
2020-05-04 23:31:36 +0000

    added 3 endpoints

--- api/views.py
+++ api/views.py
@@ -4,8 +4,6 @@ from rest_framework.response import Response
 from django.shortcuts import render
 from api.models import User,otp
 from CustomCode import string_generator,password_functions,validator,autentication
-from django.core.mail import send_mail #django email module
-from django.conf import settings
 
 # Create your views here.
 @api_view(['GET'])
@@ -16,6 +14,7 @@ def index_page(request):
     }
     return Response(return_data)
 
+#User registration endpoint
 @api_view(["POST"])
 def user_registration(request):
     try:
@@ -52,13 +51,13 @@ def user_registration(request):
                 #Save OTP
                 user_OTP =otp(user=new_userData,otp_code=code)
                 user_OTP.save()
-                send_mail(
-                        'WasteCoin OTP verification',
-                        'Hello ' + firstName + " " + lastName + "\n Your OTP confirmation code is: \n " + code + " \n Use this code to verify your registration. WasteCoin will never ask you to share this code with anyone.",
-                        settings.EMAIL_HOST_USER,
-                        [email],
-                        fail_silently=False,
-                    )
+                # send_mail(
+                #         'WasteCoin OTP verification',
+                #         'Hello ' + firstName + " " + lastName + "\n Your OTP confirmation code is: \n " + code + " \n Use this code to verify your registration. WasteCoin will never ask you to share this code with anyone.",
+                #         settings.EMAIL_HOST_USER,
+                #         [email],
+                #         fail_silently=False,
+                #     )
                 return_data = {
                     "error": "0",
                     "message":"The registration was successful",
@@ -77,11 +76,12 @@ def user_registration(request):
         }
     return Response(return_data)
 
+#User verfication
 @api_view(["POST"])
 @autentication.user_tokenid
 def user_verification(request,user_id):
     try:
-        otp_entered = request.data.get("otp")
+        otp_entered = request.data.get("otp",None)
         if otp_entered is not None and otp_entered is not "":
             user_data = otp.objects.get(user__user_id=user_id)
             otpCode,date_added = str(user_data.otp_code),user_data.date_added
@@ -102,9 +102,6 @@ def user_verification(request,user_id):
                     "message": "Incorrect OTP"
                 }
             elif otp_entered == otpCode and duration > timeLimit:
-                #We should generate another and send to the user here
-                newOTP = string_generator.numeric(6)
-                user_data.otp_code = newOTP
                 user_data.save()
                 return_data = {
                     "error": "1",
@@ -122,11 +119,54 @@ def user_verification(request,user_id):
         }
     return Response(return_data)
 
+#resend OTP
+@api_view(["POST"])
+def resend_otp(request):
+    try:
+        email_address = request.data.get('emailaddress',None)
+        if email_address is not None and email_address is not "":
+            if User.objects.filter(email =email_address).exists() == False:
+                    return_data = {
+                        "error": "1",
+                        "message": "User does not exist"
+                    }
+            else:
+                user_data = otp.objects.get(user__email=email_address)
+                #generate new otp
+                code = string_generator.numeric(6)
+                user_data.otp_code = code
+                user_data.save()
+                # send_mail(
+                #         'WasteCoin OTP verification',
+                #         'Hello '
+                #         " ""\n Your OTP confirmation code is: \n " + code + " \n Use this code to verify your registration. WasteCoin will never ask you to share this code with anyone.",
+                #         settings.EMAIL_HOST_USER,
+                #         [email_address],
+                #         fail_silently=False,
+                #     )
+                return_data = {
+                    "error": "0",
+                    "message": "OTP sent to mail",
+                    "OTP_Code": f"{code}"
+                }
+        else:
+            return_data = {
+                "error": "2",
+                "message": "Invalid Parameters"
+            }
+    except Exception as e:
+        return_data = {
+            "error": "3",
+            "message": "An error occured"
+        }
+    return Response(return_data)
+
+#User login
 @api_view(["POST"])
 def user_login(request):
     try:
-        email_phone = request.data.get("email_phone")
-        password = request.data.get("password")
+        email_phone = request.data.get("email_phone",None)
+        password = request.data.get("password",None)
         field = [email_phone,password]
         if not None in field and not '' in field:
             validate_mail = validator.checkmail(email_phone)
@@ -192,4 +232,80 @@ def user_login(request):
         }
     return Response(return_data)
 
+#send email to change password
+@api_view(["POST"])
+def password_reset(request):
+    try:
+        emailAddress = request.data.get('emailaddress',None)
+        if emailAddress is not None and emailAddress is not "":
+            if User.objects.filter(email =emailAddress).exists() == False:
+                return_data = {
+                    "error": "1",
+                    "message": "User does not exist"
+                }
+            else:
+                user_data = otp.objects.get(user__email=emailAddress)
+                generate_pin = string_generator.alphanumeric(15)
+                user_data.otp_reset_code = generate_pin
+                user_data.save()
+                # send_mail(
+                #     'WasteCoin Reset Password verification',
+                #     'Hello '" ""\n Your Reset Password pin is: \n " + generate_pin + " \n Use this code to reset you password here. WasteCoin will never ask you to share this code with anyone.",
+                #     settings.EMAIL_HOST_USER,
+                #     [emailAddress],
+                #     fail_silently=False,
+                #     )
+                return_data = {
+                    "error": "0",
+                    "message": "Successful, Email sent",
+                    "code": f"{generate_pin}"
+                }
+        else:
+            return_data = {
+                "error": "2",
+                "message": "Invalid Parameter"
+            }
+    except Exception as e:
+        return_data = {
+            "error": "3",
+            "message": "An error occured"
+        }
+    return Response(return_data)
 
+#Change password
+@api_view(["POST"])
+@autentication.user_tokenid
+def password_change(request,user_id):
+    try:
+        reset_code = request.data.get("reset_code")
+        new_password = request.data.get("new_password")
+        fields = [reset_code,new_password]
+        if not None in fields and not "" in fields:
+            #get user info 
+            user_data = User.objects.get(user_id=user_id)
+            otp_reset_code = otp.objects.get(user__user_id=user_id).otp_reset_code
+            if reset_code == otp_reset_code:
+                #encrypt password
+                encryptpassword = password_functions.generate_password_hash(new_password)
+                user_data.user_password = encryptpassword
+                user_data.save()
+                return_data = {
+                    "error": "0",
+                    "message": "Successfull, Password Changed"
+                }
+            elif resend_otp != otp_reset_code:
+                return_data = {
+                    "error": "1",
+                    "message": "Code does not Match"
+                }
+        else:
+            return_data = {
+                "error": "2",
+                "message": "Invalid Parameters"
+            }
+    except Exception as e:
+        return_data = {
+            "error": "3",
+            "message": "An error occured"
+        }
+    return Response(return_data)

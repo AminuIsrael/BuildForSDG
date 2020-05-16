fd4daf25052d3831f6ff11782b3e23367d57fa72
Aminu Israel <aminuisrael2@gmail.com>
2020-05-16 12:02:49 +0000

    Added token (#32)
    * added 3 endpoints
    
    * integrated sending email to users
    
    * Set Debug = False
    
    * made changes
    
    * changed resonse for login
    
    * added data to login response
    
    * made changes
    
    * made changes
    
    * made changes
    
    * added server.elho()
    
    * added cors header
    
    * added package to req.txt
    
    * made changes
    
    * commented code
    
    * made changes
    
    * implemeted token
    
    * set time to 2 hours

--- CustomCode/autentication.py
+++ CustomCode/autentication.py
@@ -1,17 +1,26 @@
 from rest_framework.response import Response
 from rest_framework import status
+import jwt
+from WasteCoins import settings
 
-def user_tokenid(something):
+def token_required(something):
     def wrap(request):
         try:
-            if request.META['HTTP_TOKEN_ID'] is not '':
-                token_passed = request.META['HTTP_TOKEN_ID']
-                if token_passed != "":
-                    return something(request,token_passed)
-                else:
+            if request.META['HTTP_TOKEN'] != '' and request.META['HTTP_TOKEN'] != None:
+                token_passed = request.META['HTTP_TOKEN']
+                try:
+                    data = jwt.decode(token_passed,settings.SECRET_KEY, algorithms=['HS256'])
+                    return something(request,data)
+                except jwt.exceptions.ExpiredSignatureError:
                     return_data = {
-                    'error': "1",
-                    'message': "Invalid Token"
+                        "error": "1",
+                        "message": "Token has expired"
+                        }
+                    return Response(return_data, status=status.HTTP_401_UNAUTHORIZED)
+                except:
+                    return_data = {
+                        "error": "1",
+                        "message": "Invalid Token"
                     }
                     return Response(return_data, status=status.HTTP_401_UNAUTHORIZED)
             else:
@@ -21,10 +30,11 @@ def user_tokenid(something):
                     }
                 return Response(return_data, status=status.HTTP_401_UNAUTHORIZED)
         except Exception as e:
-               return_data = {
-                   "error" : "3",
-                   "message" : str(e)
-               }
-               return Response(return_data, status=status.HTTP_401_UNAUTHORIZED)
-
+            return_data = {
+                "error" : "3",
+                "message" : "An error occured"
+                }
+            return Response(return_data, status=status.HTTP_401_UNAUTHORIZED)
     return wrap
+                
+                

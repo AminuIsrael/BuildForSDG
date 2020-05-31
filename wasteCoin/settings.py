82cd6a83992147b898fbb1e6c078b9defe0130f6
israel <aminuisrael2@gmail.com>
2020-05-31 14:57:35 +0000

    work na!!!

--- wasteCoin/settings.py
+++ wasteCoin/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-CORS_ORIGIN_ALLOW_ALL = True 
-CORS_ALLOW_CREDENTIALS = True
 
 django_heroku.settings(locals())
+CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ALLOW_CREDENTIALS = True
 #

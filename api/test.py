e25b41e480ba2927a0aff8dd6c8d3b8e225a9a3c
israel <aminuisrael2@gmail.com>
2020-07-06 10:40:29 +0000

    added tests

--- api/test.py
+++ api/test.py
@@ -1,29 +1,27 @@
 from django.test import TestCase
-from api.models import User
 from django.utils import timezone
+from api.models import User, ContactUs,otp
 
-# Create your tests here
-class UserTestCase(TestCase):
-    def setUp(self):
-        User.objects.create(user_id='h7YbViN8J1TezScP4gLE',firstname="Israel",lastname="Aminu",
-                            email='aminuisrael@gmail.com',user_phone='09017373729',
-                            user_gender="Male",user_password="123456789",user_address="Kolawole Street",
-                            user_state= "Lagos", user_LGA= "Ojo",user_country="Nigeria",date_added=timezone.now)
-
+class TestUser(TestCase):
+    create_user= User(user_id="Iral8PHTGkfuyb1hv9Xz",firstname="john",lastname="wilson",email="johnwilson@gmail.com",
+                         user_phone="09017373728",user_gender="Male",user_password="123456789",user_address="Wilson Street",
+                         user_state="Lagos",user_LGA="Ojo",user_country="Nigeria")
+    
+    def test_user_fetch(self):
+        self.assertEqual(self.create_user.user_id,"Iral8PHTGkfuyb1hv9Xz")
+        self.assertEqual(self.create_user.firstname,"john")
+        self.assertEqual(self.create_user.email,"johnwilson@gmail.com")
+        self.assertNotEqual(self.create_user.date_added,timezone.now)
+        self.assertEqual(self.create_user.role,"user")
         
-    def test_db_fetch(self):
-        "Fetching Data from the Database"
-        user = User.objects.get(user_id='h7YbViN8J1TezScP4gLE')
-        self.assertEqual(user.firstname, "Israel")
-        self.assertEqual(user.lastname,"Aminu")
-        self.assertEqual(user.email,"aminuisrael2@gmail.com")
-        self.assertEqual(user.user_phone,'09017373729')
-        self.assertEqual(user.user_gender,"Male")
-        self.assertEqual(user.user_password,"123456789")
-        self.assertEqual(user.user_address,"Kolawole Street")
-        self.assertEqual(user.user_state,"Lagos")
-        self.assertEqual(user.user_LGA,"Ojo")
-        self.assertEqual(user.user_country,"Nigeria")
-        self.assertNotEqual(user.date_added,timezone.now)
-        self.assertEqual(user.role,"user")
         
+class TestOtp(TestUser):
+    
+    def test_otp(self):
+        user_otp = otp(user=self.create_user,otp_code="123456")
+        self.assertEqual(user_otp.otp_code,"123456")
+        self.assertIs(user_otp.validated,False)
+        #Check if otp belongs to user
+        self.assertEqual(user_otp.user.firstname,"john")
+        #Check time of saving data
+        self.assertNotEqual(self.create_user.date_added,timezone.now)
